16059a08c9c7cd095f5e90e2fa41d3fda9945b55
Ayush-ghiya10 <ayush.ghiya@talentsystems.com>
2025-05-14 14:10:57 +0530

    added logs

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,19 @@ import { Elysia } from "elysia";
 
 const app = new Elysia()
   .get("/getDetails/:accessToken", async ({ params: { accessToken } }) => {
+    console.log({ accessToken });
     if (accessToken) {
-      const { data } = await axios.get(
-        "https://open.tiktokapis.com/v2/user/info?fields=open_id,union_id,avatar_url,display_name,follower_count,following_count,display_name,profile_deep_link,username,likes_count,video_count",
-        { headers: { Authorization: `Bearer ${accessToken}` } }
-      );
-      console.log(JSON.stringify(data, null, 2));
-      return data;
+      try {
+        const { data } = await axios.get(
+          "https://open.tiktokapis.com/v2/user/info?fields=open_id,union_id,avatar_url,display_name,follower_count,following_count,display_name,profile_deep_link,username,likes_count,video_count",
+          { headers: { Authorization: `Bearer ${accessToken}` } }
+        );
+        console.log(JSON.stringify(data, null, 2));
+        return data;
+      } catch (error) {
+        console.error("Error fetching user details:", error);
+        return { error: "Failed to fetch user details", errorDetails: error };
+      }
     }
   })
   .get("/", () => "Hello Elysia")
