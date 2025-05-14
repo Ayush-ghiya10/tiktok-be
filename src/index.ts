cbd2a1214e46b60c790ce293a89f31e44bf07ac5
Ayush-ghiya10 <ayush.ghiya@talentsystems.com>
2025-05-14 11:29:21 +0530

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,19 @@
+import axios from "axios";
 import { Elysia } from "elysia";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia()
+  .get("/getDetails/:accessToken", async ({ params: { accessToken } }) => {
+    if (accessToken) {
+      const { data } = await axios.get(
+        "https://open.tiktokapis.com/v2/user/info?fields=open_id,union_id,avatar_url,display_name,follower_count,following_count,display_name,profile_deep_link,username,likes_count,video_count",
+        { headers: { Authorization: `Bearer ${accessToken}` } }
+      );
+      console.log(JSON.stringify(data, null, 2));
+      return data;
+    }
+  })
+  .get("/", () => "Hello Elysia")
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
